eb2355725d8a3357a06b5a23ddb00361e797ec04
aqilusman45 <aqilusman45@gmail.com>
2019-07-27 18:03:25 +0500

    new topics

---
+++ modules/app.py
@@ -0,0 +1,15 @@
+# import my_math;
+from my_math import add;
+
+# print(my_math.add(2,4))
+print(add(3,4))
+
+from my_math import sub as subtract
+
+from my_math import * as obj;
+
+from my_math import mul, add
+
+print(subtract(2,5))
+
+print("Hello")
